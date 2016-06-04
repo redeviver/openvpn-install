a2cf626a7753144dc284d8c9084134182fd4d3c4
Richard Skumat <git@richardskumat.com>
2016-06-04 13:03:58 +0100

    Fix for ovpn2.sh where rewriting ovpn2.sh in commit https://github.com/richardskumat/openvpn-install/commit/ebdaaf25b56395f5d78fea3c53547df6b2b4261d changed 'server  ' to 'server 10.8.0.0 255.255.255.0'.

--- ovpn2.sh
+++ ovpn2.sh
@@ -135,7 +135,7 @@ key server.key
 dh dh.pem
 tls-auth ta.key 0
 topology subnet
-server 10.8.0.0 255.255.255.0
+server $IPRANGE $IPNETMASK
 ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 	echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server.conf
 	# DNS
